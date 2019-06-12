5b4511a79e2014b8669de4925b5ed164bf30e847
Alec Larson <alec.stanford.larson@gmail.com>
2019-06-12 19:42:12 -0400

    fix: local package aliases in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,47 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CheckerPlugin } = require('awesome-typescript-loader')
+// const CheckerPlugin = require('fork-ts-checker-webpack-plugin')
 const webpack = require('webpack')
 const fs = require('fs')
 
+const readJson = file => JSON.parse(fs.readFileSync(file, 'utf8'))
+
 const createAlias = (name, fallback) => (fs.existsSync(name) ? name : fallback)
+
+const buildAliases = () => {
+  const alias = {
+    react: path.resolve('./node_modules/react'),
+    'react-dom': path.resolve('./node_modules/react-dom'),
+    'react-spring$': '@react-spring/web',
+    shared: '@react-spring/shared',
+  }
+  if (fs.existsSync('../package.json')) {
+    const monorepo = readJson('../package.json')
+    if (monorepo.name == '@react-spring/lerna') {
+      // Get every package in the monorepo.
+      const dirs = [].concat(
+        ...['../packages', '../targets'].map(dir =>
+          fs
+            .readdirSync(dir)
+            .filter(p => p[0] !== '.' && p !== 'react-spring')
+            .map(p => path.join(dir, p))
+        )
+      )
+      // Redirect "@react-spring/web" to "../targets/web" for example
+      dirs.forEach(dir => {
+        const pkg = readJson(path.join(dir, 'package.json'))
+        alias[pkg.name] = createAlias(path.resolve(dir), pkg.name)
+      })
+      // Ensure "shared/globals" can be resolved.
+      alias['@react-spring/shared'] = createAlias(
+        path.resolve('../packages/shared/src'),
+        '@react-spring/shared'
+      )
+    }
+  }
+  return alias
+}
 
 module.exports = mode => {
   return {
@@ -24,7 +61,13 @@ module.exports = mode => {
         { test: /\.css$/, use: ['style-loader', 'css-loader'] },
         {
           test: /\.tsx?$/,
-          use: ['awesome-typescript-loader'],
+          use: {
+            // loader: 'ts-loader',
+            loader: 'awesome-typescript-loader',
+            options: {
+              transpileOnly: true,
+            },
+          },
         },
         {
           test: /\.(js|jsx)$/,
@@ -58,19 +101,7 @@ module.exports = mode => {
     resolve: {
       modules: [path.resolve('./'), 'node_modules'],
       extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      alias: {
-        shared: '@react-spring/shared',
-        '@react-spring/shared': createAlias(
-          path.resolve('../packages/shared/src'),
-          '@react-spring/shared'
-        ),
-        'react-spring$': createAlias(
-          path.resolve('../targets/web/src'),
-          '@react-spring/web'
-        ),
-        react: path.resolve('./node_modules/react'),
-        'react-dom': path.resolve('./node_modules/react-dom'),
-      },
+      alias: buildAliases(),
     },
     plugins: [
       new CheckerPlugin(),

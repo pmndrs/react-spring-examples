8cce0ad6fef927b80dd85b75ebf560ff4575330c
David Bismut <david.bismut@gmail.com>
2019-08-30 09:55:04 +0200

    feat: added clamp-bounce example

--- components/examples-hooks.js
+++ components/examples-hooks.js
@@ -75,6 +75,11 @@ return <animated.div style={{ transform }} children="Slide">`,
     tags: ['useSprings'],
   },
   {
+    name: 'hooks/clamp-bounce',
+    title: 'Clamp bounce',
+    tags: ['useSprings'],
+  },
+  {
     name: 'hooks/mouse-parallax',
     title: 'Mouse parallax',
     link: 'https://codesandbox.io/embed/r5x34869vq',

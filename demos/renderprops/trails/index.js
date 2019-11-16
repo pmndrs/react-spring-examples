74fef959a727be0a3ae17faae0033c895c4ee5be
David Bismut <david.bismut@gmail.com>
2019-11-17 00:09:44 +0100

    chore: switched to functional component

--- demos/renderprops/trails/index.js
+++ demos/renderprops/trails/index.js
@@ -1,39 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Trail, animated } from 'react-spring/renderprops'
 import './styles.css'
 
-export default class TrailsExample extends React.PureComponent {
-  state = { toggle: true, items: ['item1', 'item2', 'item3', 'item4', 'item5'] }
-  toggle = () => this.setState(state => ({ toggle: !state.toggle }))
-  render() {
-    const { toggle, items } = this.state
-    return (
-      <div
-        style={{
-          backgroundColor: '#247BA0',
-          position: 'relative',
-          width: '100%',
-          height: '100%',
-        }}>
-        <Trail
-          native
-          reverse={toggle}
-          initial={null}
-          items={items}
-          from={{ opacity: 0, x: -100 }}
-          to={{ opacity: toggle ? 1 : 0.25, x: toggle ? 0 : 100 }}>
-          {item => ({ x, opacity }) => (
-            <animated.div
-              className="box"
-              onClick={this.toggle}
-              style={{
-                opacity,
-                transform: x.interpolate(x => `translate3d(${x}%,0,0)`),
-              }}
-            />
-          )}
-        </Trail>
-      </div>
-    )
-  }
+const items = ['item1', 'item2', 'item3', 'item4', 'item5']
+
+export default function TrailsExample() {
+  const [toggle, setToggle] = useState(true)
+  return (
+    <div
+      style={{
+        backgroundColor: '#247BA0',
+        position: 'relative',
+        width: '100%',
+        height: '100%',
+      }}>
+      <Trail
+        native
+        reverse={toggle}
+        initial={null}
+        items={items}
+        from={{ opacity: 0, x: -100 }}
+        to={{ opacity: toggle ? 1 : 0.25, x: toggle ? 0 : 100 }}>
+        {item => ({ x, opacity }) => (
+          <animated.div
+            className="box"
+            onClick={() => setToggle(t => !t)}
+            style={{
+              opacity,
+              transform: x.interpolate(x => `translate3d(${x}%,0,0)`),
+            }}
+          />
+        )}
+      </Trail>
+    </div>
+  )
 }
